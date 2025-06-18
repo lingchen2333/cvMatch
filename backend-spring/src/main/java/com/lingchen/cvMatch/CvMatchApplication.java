82a0e34feeaa9d4f0c1a72ddccac37c36abdb1ba
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-06-18 21:54:43 +0100

    first commit

---
+++ backend-spring/src/main/java/com/lingchen/cvMatch/CvMatchApplication.java
@@ -0,0 +1,13 @@
+package com.lingchen.cvMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvMatchApplication.class, args);
+	}
+
+}
---
+++ backend-spring/src/test/java/com/lingchen/cvMatch/CvMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingchen.cvMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
